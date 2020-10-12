40fbbb8852b43dd4688a6356cd6741cd0f76eafa
Gaurav Dalal <gdalal49@gmail.com>
2020-10-12 19:07:01 +0530

    Second Commmit

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
+const express    = require("express"),
+      app        = express(),
+      bodyParser = require("body-parser"),
+      mongoose   = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/yelp_camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('Connected to DB!')).catch(error => console.log(error.message));
+
+//Scema Setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground",campgroundSchema);
+// Campground.create(
+//     {
+//         name: "Mt Abu", 
+//         image: "https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&h=350",
+//         description: "This is located in mout Abu, 100KMs away from udaipur. It has a beutiful sunset point."
+//     }, function(err, campground){
+//         if(err){
+//             console.log(err);
+//         }else{
+//             console.log("Newly Created Campground");
+//         }
+
+//     }
+// );
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
@@ -10,32 +38,48 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
-var campgrounds = [
-    {name: "Mt Abu", image: "https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "Manali Heights", image: "https://images.pexels.com/photos/1539225/pexels-photo-1539225.jpeg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "Shilong Valley", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "Shilma Trails", image: "https://pixabay.com/get/55e8dc404f5aab14f1dc84609620367d1c3ed9e04e507749772c78d09745c3_340.jpg"},
-    {name: "Himalayan Camp", image: "https://images.pexels.com/photos/2603681/pexels-photo-2603681.jpeg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "River Sides", image: "https://pixabay.com/get/53e4d1424b56a814f1dc84609620367d1c3ed9e04e507749772c78d09745c3_340.jpg"},
-    {name: "Rafters Camp", image: "https://images.pexels.com/photos/1840394/pexels-photo-1840394.jpeg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "Mount Abu Trails", image: "https://images.pexels.com/photos/2516423/pexels-photo-2516423.jpeg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "Himalayan Wild Camp", image: "https://images.pexels.com/photos/2609954/pexels-photo-2609954.jpeg?auto=compress&cs=tinysrgb&h=350"}
-];
-
+//INDEX Route
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("index", {campgrounds: allCampgrounds});
+
+        }
+    });
 });
 
+//NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res){
     res.render("new");
 })
 
+//CREATE - add new campground to db
 app.post("/campgrounds", function(req, res){
     let name = req.body.name;
     let image = req.body.image;
-    let newCampGround = {name: name, image: image};
-    campgrounds.push(newCampGround);
-    res.redirect("/campgrounds");
+    let description = req.body.description;
+    let newCampGround = {name: name, image: image, description: description};
+    Campground.create(newCampGround, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/campgrounds");
+        }
+    })
+});
+
+//SHOW -Shows more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("show", {campground: foundCampground});
+        }
+
+    });
 });
 
 app.listen(4000, function(){
